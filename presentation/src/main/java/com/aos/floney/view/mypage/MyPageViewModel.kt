49fa4f46019b6a7920523ead26cc38ffdc9a05d4
wnsgml517 <wnsgml517@naver.com>
2025-04-16 17:31:16 +0900

    [refactor/#82] MypageViewModel 안쓰는 코드 삭제

--- presentation/src/main/java/com/aos/floney/view/mypage/MyPageViewModel.kt
+++ presentation/src/main/java/com/aos/floney/view/mypage/MyPageViewModel.kt
@@ -26,46 +26,7 @@ import java.util.Locale
 
 @HiltViewModel
 class MyPageViewModel @Inject constructor(
-    private val prefs: SharedPreferenceUtil,
-    private val mypageSearchUseCase: MypageSearchUseCase,
-    private val recentBookKeySaveUseCase: RecentBookkeySaveUseCase
 ): BaseViewModel() {
-
-    // 회원 정보
-    private var _mypageInfo = MutableLiveData<UiMypageSearchModel>()
-    val mypageInfo: LiveData<UiMypageSearchModel> get() = _mypageInfo
-
-    // 가계부 리스트
-    private var _mypageList = MutableLiveData<List<MyBooks>>()
-    val mypageList: LiveData<List<MyBooks>> get() = _mypageList
-
-    // 메일 문의하기 페이지
-    private var _mailPage = MutableEventFlow<Boolean>()
-    val mailPage: EventFlow<Boolean> get() = _mailPage
-
-    // 공지사항 페이지
-    private var _noticePage = MutableEventFlow<Boolean>()
-    val noticePage: EventFlow<Boolean> get() = _noticePage
-
-    // 회원 정보 페이지
-    private var _informPage = MutableEventFlow<Boolean>()
-    val informPage: EventFlow<Boolean> get() = _informPage
-
-    // 설정 페이지
-    private var _settingPage = MutableEventFlow<Boolean>()
-    val settingPage: EventFlow<Boolean> get() = _settingPage
-
-    // 개인 정보 페이지
-    private var _privatePage = MutableEventFlow<Boolean>()
-    val privatePage: EventFlow<Boolean> get() = _privatePage
-
-    // 이용 약관 페이지
-    private var _usageRightPage = MutableEventFlow<Boolean>()
-    val usageRightPage: EventFlow<Boolean> get() = _usageRightPage
-
-    // 가계부 추가 BottomSheet
-    private var _bookAddBottomSheet = MutableEventFlow<Boolean>()
-    val bookAddBottomSheet: EventFlow<Boolean> get() = _bookAddBottomSheet
 
     // 내역추가
     private var _clickedAddHistory = MutableEventFlow<String>()
