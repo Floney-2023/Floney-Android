258f5974d3f0da286308daa752f6509d492f9eab
wnsgml517 <wnsgml517@naver.com>
2025-05-09 17:53:27 +0900

    [fix/#82] interceptor 만료 후 재접속 할 경우 홈 화면으로 이동 로직 변경
    만료된 상태일 경우, 스플래시 > 홈 화면 이동이 아닌 로그인 화면으로 이동하도록 한다.

--- presentation/src/main/java/com/aos/floney/view/splash/SplashViewModel.kt
+++ presentation/src/main/java/com/aos/floney/view/splash/SplashViewModel.kt
@@ -1,9 +1,16 @@
 package com.aos.floney.view.splash
 
+import com.aos.data.util.AuthInterceptor
 import com.aos.floney.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class SplashViewModel @Inject constructor(): BaseViewModel() {
+class SplashViewModel @Inject constructor(
+    private val authInterceptor: AuthInterceptor
+): BaseViewModel() {
+
+    fun getSessionExpiredFlag(): Boolean {
+        return authInterceptor.getSessionExpiredFlag()
+    }
 }
